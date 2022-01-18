b4b15fe05c57b2cecc9952676de0a0f2c810e03c
Cameron Burridge <cmburridge4022@gmail.com>
2022-01-17 21:05:58 -0700

    Finished movement tutorial and added platforms

--- DumRocToss/Assets/Scripts/DragAndShoot.cs
+++ DumRocToss/Assets/Scripts/DragAndShoot.cs
@@ -13,22 +13,63 @@ public class DragAndShoot : MonoBehaviour
     public Vector2 minPower;
     public Vector2 maxPower;
 
+    private LineBehavior lb;
+
     private Camera cam;
     private Vector2 force;
     private Vector3 startPoint;
     private Vector3 endPoint;
+    
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+    public float jumpCount;
+    public float jumpCountMax;
+
 
     private void Start()
     {
         cam = Camera.main;
+        lb = GetComponent<LineBehavior>();
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true)
         {
-            startPoint = Input.mousePosition;
-            Debug.Log(startPoint);
+            jumpCount = jumpCountMax;
+        }
+
+        if (jumpCount > 0 && Input.GetMouseButtonDown(0))
+        {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15; 
+        }
+
+        if (jumpCount > 0 && Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15; 
+            lb.RenderLine(startPoint, currentPoint);
+        }
+        
+        if (jumpCount > 0 && Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+            lb.EndLine();
+            
+            jumpCount -= 1;
         }
     }
 }
