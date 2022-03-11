397e103aa66af182eb5b15453b7c871c5c0c421b
Cameron Burridge <cmburridge4022@gmail.com>
2022-03-10 18:30:44 -0700

    Changes to art and desert level layout, adding more environment

---
+++ DumRocToss/Assets/AnimationOnTrigger.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOnTrigger : MonoBehaviour
+{
+    public Animator anim;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("works");
+        anim.Play("Shake");
+    }
+}
